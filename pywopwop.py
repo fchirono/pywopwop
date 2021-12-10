bf830b9d3cc7e342a4cc6541a0a28aed9cf45602
fchirono <fchirono@gmail.com>
2021-12-10 18:27:36 +0000

    Adds import of read_geom_file in pywopwop

--- pywopwop.py
+++ pywopwop.py
@@ -25,7 +25,8 @@ from readers_and_writers import initial_check, read_block, write_block, \
 from zones import Zone, StructuredZone, StructuredConstantGeometry, \
     StructuredConstantLoading
 
-from sigma_paraview import extract_var_names, read_fn_file, write_p3d_file
+from sigma_paraview import extract_var_names, read_fn_file, read_geom_file, \
+    write_p3d_file
 
 
 # %% #######################################################################
