7793cf7d201cd44de88dd49206015b5bc5138f78
fchirono <fchirono@gmail.com>
2021-12-16 12:00:45 +0000

    Clearly document the two possible approaches

--- test_files/sigma_paraview.py
+++ test_files/sigma_paraview.py
@@ -11,14 +11,14 @@ For vector data, each component (x,y,z) is output as a separate variable. These
 must be built into a vector array using, for example, the Python Calculator in
 Paraview [1].
 
-For example, for three velocity components 'Vx', 'Vy', 'Vz':    
+For example, for three velocity components 'Vx', 'Vy', 'Vz':
     - Load the .p3d file in Paraview;
-    
+
     - Add a new filter via 'Filters' - 'Alphabetical' - 'Python Calculator';
-    
+
     - In Python Calculator, set the 'Expression' to 'make_vector(Vx, Vy, Vz)'
     and the 'Array Name' to e.g. 'Velocity'. Click 'Apply'.
-    
+
 You should now have a new variable called 'Velocity' available for selection.
 You can now choose to color the visualization by, e.g., vector magnitude in the
 'Coloring' section.
@@ -26,9 +26,9 @@ You can now choose to color the visualization by, e.g., vector magnitude in the
 
 To visualize the vectors as arrows:
     - Add a new glyph via 'Filters' - 'Alphabetical' - 'Glyph';
-    
+
     - In 'Glyph Source' - 'Glyph Type', select 'Arrow';
-    
+
     - In 'Orientation' - 'Orientation Array', select your vector - e.g. 'V'.
 
 
@@ -39,7 +39,7 @@ informative coloring scheme in 'Coloring' - e.g., vector magnitude.
 For efficiency, you probably only need to visualize every few points. In
 'Masking' - 'Glyph Mode', select 'Every Nth point' and in 'Stride' select
 a number between, e.g., 2 and 20.
-    
+
 
 Author:
     Fabio Casagrande Hirono
@@ -68,10 +68,16 @@ filename_fn = path_to_sigma + '/sigma.fn'
 # path for file output - here we create a new folder called 'timesteps' to
 # store the newly created single-timestep (.x, .fn) and Paraview reader (.p3d)
 # files
-path_output = path_to_sigma + '/timesteps/'
+path_output = path_to_sigma + '/timesteps2/'
 
 
 # %% #######################################################################
+# Approach 1: merge zones' data by time step index (i.e. all zones' first step
+# are used to create the first single-timestep output file). This leads to
+# mismatched source times, as zones have different values for their source
+# times.
+#
+# This section has not been refactored into a neat, single function call!
 
 # # extract names of Sigma variables from .nam file
 # var_names = PWW.extract_sigma_var_names(filename_nam)
@@ -86,7 +92,10 @@ path_output = path_to_sigma + '/timesteps/'
 # p3d_filename = 'read_sigmasurfaces_case1'
 # PWW.write_p3d_file(p3d_filename, path_output, source_time, var_names)
 
-# %%
+# %% #######################################################################
+# Approach 2: align zones to their nearest source time steps, discarding steps
+# that only exist for some zones. This reduces the temporal/spatial
+# misalignment and leads to nicer visualisations in Paraview.
 
 PWW.process_sigma_files(filename_geom, filename_fn, filename_nam,
                         path_output)
