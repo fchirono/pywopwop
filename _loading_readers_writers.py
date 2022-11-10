a311bdd643ff1d47ffaac4331a3c3522634e37c9
fchirono <fchirono@gmail.com>
2022-11-10 13:47:43 +0000

    Adds aperiodic case to write_loading_header

--- _loading_readers_writers.py
+++ _loading_readers_writers.py
@@ -467,7 +467,19 @@ def _write_loading_header(self, loading_filename):
 
             # -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
             elif self.loading_time_type == 'aperiodic':
-                pass
+
+                # for each zone containing data...
+                for nz in self.zones_with_loading_data:
+
+                    # write name (32-byte string)
+                    write_string(file, self.zones[nz].loading_name, 32)
+
+                    # write number of timesteps
+                    write_binary(file, self.Nt)
+
+                    # write iMax and jMax (4 byte ints)
+                    write_binary(file, self.zones[nz].iMax)
+                    write_binary(file, self.zones[nz].jMax)
             # -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
             else:
                 # TODO: implement non-constant, non-aperiodic functional data header
