16b7b24800c1310240fc4002ad1482c455cd26b7
fchirono <fchirono@gmail.com>
2022-11-17 14:08:22 +0000

    Remove unused imports in XX_readers_writers

--- _loading_readers_writers.py
+++ _loading_readers_writers.py
@@ -17,18 +17,11 @@ Author:
 import numpy as np
 
 from ._consts_and_dicts import MAGICNUMBER, ENDIANNESS, VALUE_LENGTH, \
-    IS_SIGNED, RESERVED_DIGIT, reverse_dict, geom_dict, structured_dict, \
-    loading_time_dict, geometry_time_dict, structured_header_length, \
-    centered_dict, loading_data_dict, ref_frame_dict, float_dict, iblank_dict
+    reverse_dict, structured_dict, loading_time_dict, structured_header_length, \
+    centered_dict, loading_data_dict, ref_frame_dict, float_dict
 
 from ._binary_readers_writers import initial_check, read_block, write_block, \
-    read_IBLANKblock, read_int, read_float, write_binary, write_string, \
-    read_string
-
-from ._zones import Zone, StructuredZone, \
-    StructuredConstantGeometry, StructuredAperiodicGeometry, \
-    StructuredConstantLoading, StructuredAperiodicLoading
-
+    read_int, read_float, write_binary, write_string, read_string
 
 
 # ##########################################################################
