db3a66fc677346e7042397ffdc44cb0253d9fb02
fchirono <fchirono@gmail.com>
2022-11-28 16:23:35 +0000

    Update 'create_data_manually' example to refactored code

--- test_files/create_data_manually.py
+++ test_files/create_data_manually.py
@@ -1,30 +1,31 @@
 """
 Test script for pywopwop - https://github.com/fchirono/pywopwop
 
-    This script creates a toy dataset manually, containing a
+    This script creates a toy dataset manually and shows how to introduce
+    data into a PWWPatch instance.
 
-    rites this data to a set
-    of geometry and loading PSU-WOPWOP v1.0 files.
+    The example dataset is a collection of structured meshes, with constant
+    geometry and constant loading. It contains random values for mesh
+    coordinates, normal vectors and surface pressures.
 
 
 Author:
     Fabio Casagrande Hirono
-    Oct 2021
+    Nov 2022
 """
 
 import numpy as np
 import pywopwop as PWW
 
-# %%
-# create toy dataset - random numbers for mesh coordinates, normal vectors and
-# surface pressures
+write_files = False
+
+# %% create toy dataset with structured geometry
 
 name = 'Zone 1'
-
 iMax = 20
 jMax = 61
 
-
+# uses random numbers for mesh coordinates, normal vectors and surface pressures
 rng = np.random.default_rng()
 
 XYZ_coord = rng.normal(size=(3, iMax, jMax))
@@ -32,46 +33,68 @@ normal_coord = rng.normal(size=(3, iMax, jMax))
 loading_data = rng.normal(size=(iMax, jMax))
 
 
-# %%
-# initialize new instance
+# %% initialize PWWPatch instance
 myWopwopData = PWW.PWWPatch()
 
-myWopwopData.geom_type = 'geometry'
+# add general header info
 myWopwopData.is_structured = True
-myWopwopData.geometry_time_type = 'constant'
 myWopwopData.centered_type = 'node'
 myWopwopData.float_type = 'single'
-myWopwopData.iblank_included = 'n'
-myWopwopData.units_string = 'Pa'
+myWopwopData.has_iblank = False
+myWopwopData.set_units_string('Pa')
 
-# 1024-byte comment string for geometry file
-myWopwopData.geometry_comment = 'Test file for pywopwop - geometry'
+# add geometry info
+myWopwopData.geometry_type = 'geometry'
+myWopwopData.geometry_time_type = 'constant'
+myWopwopData.set_geometry_comment('Test file for pywopwop - geometry')
 
-
+# add loading info
 myWopwopData.loading_time_type = 'constant'
 myWopwopData.loading_data_type = 'surf_pressure'
 myWopwopData.loading_ref_frame = 'blade_fixed'
-# 1024-byte comment string for loading file
-myWopwopData.loading_comment = 'Test file for pywopwop - loading'
+myWopwopData.set_loading_comment('Test file for pywopwop - loading')
 
 
-# adds zone 1 info - mesh coords, normal coords, loading data
-myWopwopData.add_StructuredZone(name, XYZ_coord, normal_coord,
+# %% Adds structured zones, one at a time
+
+# *****************************************************************************
+# adds zone 1 info - name: 'Zone 1'
+#   --> adds random mesh coords, normal coords, loading data
+#   --> has thickness noise
+#   --> has loading data
+
+myWopwopData.add_StructuredZone(name,
+                                XYZ_coord, normal_coord,
                                 calc_thickness_noise=True,
                                 loading_data=loading_data)
 
-# add zone 2 - no loading data
-myWopwopData.add_StructuredZone(name[:-1] + '2', XYZ_coord+1, normal_coord+1,
+# *****************************************************************************
+# adds zone 2 info - name: 'Zone 2'
+#   --> XYZ coords and normal coords are identical to zone 1, plus 1 unit
+#   --> has thickness noise
+#   --> doesn't have loading data
+
+myWopwopData.add_StructuredZone(name[:-1] + '2',
+                                XYZ_coord+1, normal_coord+1,
                                 calc_thickness_noise=True)
 
-# add zone 3 - loading data, no thickness noise
-myWopwopData.add_StructuredZone(name[:-1] + '3', XYZ_coord+2, normal_coord+2,
+# *****************************************************************************
+# adds zone 1 info - name: 'Zone 3'
+#   --> XYZ coords, normal coords, and loading data are identical to zone 1,
+#       plus 2 units
+#   --> doesn't have thickness noise
+#   --> has loading data
+
+myWopwopData.add_StructuredZone(name[:-1] + '3',
+                                XYZ_coord+2, normal_coord+2,
                                 calc_thickness_noise=False,
                                 loading_data=loading_data+2)
 
-myWopwopData.print_info()
+# %% # print info, write files
 
-# %% write files
+# print info about PWWPatch, including zones' info
+myWopwopData.print_info(zones_info=True)
 
-myWopwopData.write_geometry_file('BladeGeometry.dat')
-myWopwopData.write_loading_file('BladeLoading.dat')
+if write_files:
+    myWopwopData.write_geometry_file('BladeGeometry.dat')
+    myWopwopData.write_loading_file('BladeLoading.dat')
