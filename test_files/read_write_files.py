324ed23f8e3de7e43dcd9b585d3ed65f969f70fe
fchirono <fchirono@gmail.com>
2022-11-28 17:10:20 +0000

    Add options for other test cases in 'read_write_files' example

--- test_files/read_write_files.py
+++ test_files/read_write_files.py
@@ -1,5 +1,5 @@
 """
-Test script for pywopwop - https://github.com/fchirono/pywopwop
+pywopwop - https://github.com/fchirono/pywopwop
 
     This script reads a pair of geometry and loading PSU-WOPWOP files, and
     rewrites the same data using different file names.
@@ -11,7 +11,7 @@ Test script for pywopwop - https://github.com/fchirono/pywopwop
 
 Author:
     Fabio Casagrande Hirono
-    Oct 2021
+    Nov 2022
 """
 
 import numpy as np
@@ -19,26 +19,45 @@ rng = np.random.default_rng()
 
 import pywopwop as PWW
 
+write_files = False
 
 # %% Define directory containing geometry and loading files, plus filenames
 
-wopwop_dir = '../../../PSU-WOPWOP_v3.4.4/case1/'
-geometry_filename = 'gyrodyne.dat'
-loading_filename = 'gyrodyneLoading.dat'
+# *****************************************************************************
+# # CASE 1: constant geometry blade, constant compact loading vectors [OK]
+# wopwop_dir = '../../OneDrive - University of Salford/PSU-WOPWOP_v3.4.4/case1/'
+# geometry_filename = 'gyrodyne.dat'
+# loading_filename = 'gyrodyneLoading.dat'
 
-# # aperiodic loading not supported yet - loading file will not be identical
-# wopwop_dir = '../../../PSU-WOPWOP_v3.4.4/case5/'
-# geometry_filename = 'constGeo_short.dat'
-# loading_filename = 'AperLoadingShort.dat'
 
-# %%
-# initialize new instance, read files
+# *****************************************************************************
+# # CASE 2: permeable surface with constant geometry, periodic flow data [NOT IMPLEMENTED]
+# wopwop_dir = '../../OneDrive - University of Salford/PSU-WOPWOP_v3.4.4/case2/'
+# geometry_filename = 'reduced_geometry.dat'
+# loading_filename = 'reduced_flow_data.dat'
+
+
+# *****************************************************************************
+# # CASE 4: constant [?] surface geometry, periodic loading vectors [NOT IMPLEMENTED]
+# wopwop_dir = '../../OneDrive - University of Salford/PSU-WOPWOP_v3.4.4/case4/'
+# geometry_filename = 'HART_Blade.dat'
+# loading_filename = 'HART_Bladeloading.dat'
+
+
+# *****************************************************************************
+# CASE 5: constant geometry, aperiodic loading
+wopwop_dir = '../../OneDrive - University of Salford/PSU-WOPWOP_v3.4.4/case5/'
+geometry_filename = 'constGeo_short.dat'
+loading_filename = 'AperLoadingShort.dat'
+
+# %% initialize new instance, read files
 myWopwopData = PWW.PWWPatch()
 myWopwopData.read_geometry_file(wopwop_dir + geometry_filename)
 myWopwopData.read_loading_file(wopwop_dir + loading_filename)
 
-myWopwopData.print_info()
+myWopwopData.print_info(zones_info=True)
 
-# rewrite same data in different filenames
-myWopwopData.write_geometry_file('geometry_file.dat')
-myWopwopData.write_loading_file('loading_file.dat')
+# %% rewrite same data using different filenames
+if write_files:
+    myWopwopData.write_geometry_file(wopwop_dir + 'geometry_file.dat')
+    myWopwopData.write_loading_file(wopwop_dir + 'loading_file.dat')
